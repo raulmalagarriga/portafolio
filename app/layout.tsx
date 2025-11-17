ef0f82fe5150ce8bfcdd0f06be4ea2fcd0a76bf3
Raúl Malagarriga <raulmalagarriga@hotmail.com>
2025-11-17 10:21:48 -0400

    Handle hydration mismatch for theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=0.8" />
